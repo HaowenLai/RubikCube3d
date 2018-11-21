31d73012e6125cc0086d6f7a3741747a281d9099
Derek Lai <this_is_my_mail2@163.com>
2018-11-22 00:07:58 +0800

    Initial commit

---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour 
+{
+    Transform blueFace;
+    void Start()
+    {
+        blueFace = transform.GetChild(2);
+    }
+    
+    void Update()
+    {
+        blueFace.Rotate(new Vector3(0, 0, 1), 20.0f * Time.deltaTime);
+        transform.Rotate(new Vector3(1, 1, 1), 30.0f*Time.deltaTime);
+    }
+
+}
